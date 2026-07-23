6baab8ee59fe44106720964aae352b8f541ee940
Pascuense <cristobalalisteg@gmail.com>
2026-07-23 10:19:12 +0200

    Añadir avisos iniciales

---
+++ data/announcements.js
@@ -0,0 +1,1 @@
+window.UTEBO_ANNOUNCEMENTS = [{id:'demo',level:'info',title:'Versión demostrativa',message:'Consulta siempre los avisos oficiales del Ayuntamiento antes de desplazarte.',active:true}];
